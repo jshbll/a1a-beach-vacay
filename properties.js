bf688c64cdb95b8d136a4e93425349ca48396948
Joshua ball <hello@mrjoshball.com>
2024-09-28 09:36:53 -0400

    repair url

--- properties.js
+++ properties.js
@@ -129,8 +129,8 @@ function createListingElement(listing) {
     .toLowerCase()
     .replace(/\s+/g, '-')          // Replace whitespace with hyphens
     .replace(/[^a-z0-9\-]/g, '')   // Remove invalid characters
-    .replace(/^-+|-+$/g, '')       // Remove leading or trailing hyphens
-    .replace(/\s-\s/g, '---')   // Replace a single hyphen with spaces on both sides with three hyphens
+   // .replace(/^-+|-+$/g, '')       // Remove leading or trailing hyphens
+    //.replace(/\s-\s/g, '---')   // Replace a single hyphen with spaces on both sides with three hyphens
     }" class="rental-card w-inline-block">              <div class="card-top-2">
                   <div class="room-quick-info">
                       <div class="intro-card-stats">
